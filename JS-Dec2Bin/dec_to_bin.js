1d718263f8748d394dadc3cdf0c8ef83a55a6972
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-16 01:31:15 +0000

    Adiciona Finalização do Projeto.

--- JS-Dec2Bin/dec_to_bin.js
+++ JS-Dec2Bin/dec_to_bin.js
@@ -2,9 +2,33 @@ var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 buttonElement.onclick = function() {
-    alert(dec2bin(parseInt(inputElement.value)));
+    var resultadoElement = document.querySelector('#resultado');
+    var pElement = document.createElement('p');
+    var pText = document.createTextNode(dec2bin(parseInt(inputElement.value)));
+
+    resultadoElement.innerHTML = '';  // Reseta resultado
+
+    pElement.appendChild(pText);
+    resultadoElement.appendChild(pElement);
 }
 
 function dec2bin(decimal) {
-    return decimal + 1;
+    var bin = [];
+
+    if(decimal == 0) {
+        return decimal;
+    }
+    while(decimal >= 1) {
+        if(decimal == 1) {
+            bin.push(decimal);
+            break;
+        }
+
+        bin.push(Math.floor(decimal % 2));
+        decimal /= 2;
+    }
+
+    var resultado = bin.reverse().join('');
+
+    return resultado;
 }
